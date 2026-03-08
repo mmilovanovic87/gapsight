2a5ce681e7fd875755dbc30b98b0604daf086cb9
Miki <mmilovanovic87@gmail.com>
2026-03-08 21:12:12 +0100

    feat: add recommendation engine and fraud detection templates

--- src/components/assessment/TemplateStep.jsx
+++ src/components/assessment/TemplateStep.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 const BADGE_COLORS = {
   yellow: 'bg-yellow-100 text-yellow-800 border-yellow-300',
   red: 'bg-red-100 text-red-800 border-red-300',
+  orange: 'bg-orange-100 text-orange-800 border-orange-300',
   green: 'bg-green-100 text-green-800 border-green-300',
   blue: 'bg-blue-100 text-blue-800 border-blue-300',
 };
